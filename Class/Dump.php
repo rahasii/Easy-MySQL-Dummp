78a53e2a6b2c746ef527d65b980422ef6aeea605
rahasii <rahasii.2010@gmail.com>
2019-04-01 23:57:34 +0900

    Dump::dumpAll()をignoreを考慮する処理に変更 設定ファイルから無視テーブルを読み込み処理を追加

--- Class/Dump.php
+++ Class/Dump.php
@@ -11,6 +11,8 @@ class Dump
     private $_port;
     private $_database;
 
+    private $_ignores;
+
     private $_dumpFileName;
 
     function __construct($dumpType = null)
@@ -24,6 +26,7 @@ class Dump
 
         $this->_frontPart = $this->_command . $this->_user . $this->_password . $this->_host . $this->_port . $this->_database;
         $this->_dumpFileName = $this->makeDumpFileName($dumpType);
+        $this->_ignores = $this->readIgnoreSettings();
     }
 
     public function makeDumpFileName($dumpType)
@@ -38,14 +41,27 @@ class Dump
         return PARTIAL_SETTINGS_DIR . '/' . $settingsName . '.text';
     }
 
+    public function readIgnoreSettings()
+    {
+        $file = fopen(__DIR__."/../IgnoreSettings/ignores.txt", "r");
+        $ignores = '';
+        if ($file) {
+            while ($line = fgets($file)) {
+                $ignores .= '--ignore-table=' . DB_NAME . '.' . $line . ' ';
+            }
+        }
+        fclose($file);
+        return $ignores;
+    }
+
     public function dumpFull()
     {
-
+        return $this->_frontPart . ' > ' . $this->_dumpFileName;
     }
 
     public function dumpAll()
     {
-        return $this->_frontPart . ' > ' . $this->_dumpFileName;
+        return $this->_frontPart . ' ' . $this->_ignores . ' > ' . $this->_dumpFileName;
     }
 
     public function dumpPartial($settingFile)
